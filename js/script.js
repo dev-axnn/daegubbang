2425083c9714827f45633749b9d62ca74d2fba32
dev-axnn <dev.axnn@gmail.com>
2022-08-23 18:05:33 +0900

    메뉴, 고정리모콘, aos 등 완성

--- js/script.js
+++ js/script.js
@@ -1,22 +1,8 @@
 window.onload = function(){
-  // 장바구니 카운트 기능
-  // let headerCartNumber = $('.header-cart-number');
-  // let cartCout = 0;
-  // headerCartNumber.html(cartCout);
-  // let newItem = $('.new-item a');
-  // $.each(newItem, function(index, item) {
+  AOS.init();
 
-  //   $(this).click(function(event){
-  //     // a 태그라면 href 를 막는다.
-  //     event.preventDefault();
-  //     cartCout ++;
-  //     headerCartNumber.html(cartCout);
-  //   });
-
-  // });
-
+  // 헤더 변형 및 고정
   let headerBtm = $('.header-btm');
-
   $(window).scroll(function(){
     // 스크롤바 상단의 위치
     let scY = $(window).scrollTop();    
@@ -27,5 +13,31 @@ window.onload = function(){
       headerBtm.removeClass('header-scroll');
       $('body').css('padding-top', 0);
     }
+  });
+
+  // FIX CONTROLLER:: 위, 아래로
+  let goTop = $('.fc-moving-gotop');
+  goTop.click(function(){
+    $('html').animate({scrollTop:0}, 1000);
+  });
+
+  let goBtm = $('.fc-moving-gobtm');
+  let btmY = $(document).height();
+  goBtm.click(function(){
+    $('html').animate({scrollTop:btmY}, 1000);
+  });
+
+  // 장바구니 카운트:: 상품 내 카트 버튼 누르면 헤더 카트의 갯수 변경
+  let headerCartNumber = $('.header-cart-number');
+  let cartCount = 0;
+  headerCartNumber.html(cartCount);
+  let addCart = $('.shopping-cart');
+  $.each(addCart, function(index, item) {
+    $(this).click(function(event){
+      event.preventDefault();
+      cartCount ++;
+      headerCartNumber.html(cartCount);
+    });
+
   });
 };
